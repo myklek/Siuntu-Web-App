536663efb9d4db1f1a7c4319db7838ebfe6d7cc6
Mykolas <mykolas2000@gmail.com>
2024-06-06 18:28:43 +0300

    ip fix

--- lib/utils/consts.dart
+++ lib/utils/consts.dart
@@ -1,2 +1,2 @@
-const String ip = 'localhost:8080';
-// const String ip = String.fromEnvironment('SERVER_IP');
+// const String ip = 'localhost:8080';
+const String ip = String.fromEnvironment('SERVER_IP');
